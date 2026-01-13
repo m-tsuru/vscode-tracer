dc18cf6db09e3d66b35a209a343caf2f3586060d
m-tsuru <147146974+m-tsuru@users.noreply.github.com>
2026-01-14 03:28:24 +0900

    format: settings

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,28 +1,34 @@
 import typescriptEslint from "typescript-eslint";
 
-export default [{
-    files: ["**/*.ts"],
-}, {
-    plugins: {
-        "@typescript-eslint": typescriptEslint.plugin,
+export default [
+    {
+        files: ["**/*.ts"],
     },
+    {
+        plugins: {
+            "@typescript-eslint": typescriptEslint.plugin,
+        },
 
-    languageOptions: {
-        parser: typescriptEslint.parser,
-        ecmaVersion: 2022,
-        sourceType: "module",
+        languageOptions: {
+            parser: typescriptEslint.parser,
+            ecmaVersion: 2022,
+            sourceType: "module",
+        },
+
+        rules: {
+            "@typescript-eslint/naming-convention": [
+                "warn",
+                {
+                    selector: "import",
+                    format: ["camelCase", "PascalCase"],
+                },
+            ],
+
+            curly: "warn",
+            eqeqeq: "warn",
+            "no-throw-literal": "warn",
+            semi: "warn",
+            indent: ["error", 4],
+        },
     },
-
-    rules: {
-        "@typescript-eslint/naming-convention": ["warn", {
-            selector: "import",
-            format: ["camelCase", "PascalCase"],
-        }],
-
-        curly: "warn",
-        eqeqeq: "warn",
-        "no-throw-literal": "warn",
-        semi: "warn",
-        indent: ["error", 4],
-    },
-}];
+];
